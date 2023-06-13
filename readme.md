f7e786d47db726917b3b93e5cd3d34ce085b0627
Jefferson <136135295+Jeff-dev3@users.noreply.github.com>
2023-06-13 15:56:30 -0300

    Adicionando arquivos ao novo repositório

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Pagina web simples com formulário
+
+## Formulário sem validação.
+
+### Linguagens utilizadas.
+
+- Html
+- Css 
+- Js
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+let mensagem = ('Obrigado pela confiaça, entraremos em contato por email!');
+
+function confirmarEnvio(){
+    let botaoEnviar = document.getElementById('enviar');
+}
+
+ confirmarEnvio(alert(mensagem));
+
